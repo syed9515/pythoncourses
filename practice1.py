0ffc20532a45b4502cfcc157425235c5f7c57086
unknown <shaiksyed9515@gmail.com>
2023-10-06 16:51:44 +0530

    my first commit

---
+++ practice1.py
@@ -0,0 +1,9 @@
+print("hellow world")
+x=10
+y=10.1
+z="syed"
+#comment
+print("python is a script,variable,object oriented high level programming language")
+print(x,type(x))
+print(y,type(y))
+print(z,type(z))
