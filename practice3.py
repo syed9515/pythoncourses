aaa0a04e327fa535ea24612ef1ca3034ff23469c
unknown <shaiksyed9515@gmail.com>
2023-10-07 17:04:22 +0530

    type casting

---
+++ practice3.py
@@ -0,0 +1,102 @@
+print("apan_shaik_9515")
+       #type casting
+
+
+print('type casting')
+
+"""
+type casting is use to convert int==>float,int==>str,float==>str,float==>int,str==>int and str==>float.
+"""
+
+       #int==>float
+
+print('int==>float')
+
+x=100
+y=float(x)
+print(type(y),y)
+
+       #int==>str
+
+print('int==>str')
+
+x=150
+y=str(x)
+print(type(y),y)
+
+       #float==>str
+
+print('float==>str')
+
+x=200.0
+y=str(x)
+print(type(y),y)
+
+       #float==>int
+
+print('float==>int')
+
+x=250.50
+y=int(x)
+print(type(y),y)
+
+       #str==>int
+
+print('str==>int')
+
+x='300'
+y=int(x)
+print(type(y),y)
+
+       #str==>float
+
+print('str==>float')
+
+x='350.50'
+y=float(x)
+print(type(y),y)
+
+
+       #sum-str==>str
+
+print('sum-str+str')
+
+x='volley'
+y='ball'
+z=(x+y)
+print(type(z),z)
+
+       #sum-int==>float
+
+print('sum-int+float')
+
+x=500
+y=50.00
+z=(x+y)
+print(type(z),z)
+
+       #sum-float+int
+
+print('sum-float+int')
+
+x=100.0
+y=100
+z=(x+y)
+print(type(z),z)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
